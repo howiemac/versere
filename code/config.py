da1a6f66dfb01f3bbaf9e728b23598a759ac3602
howie <howiemac@gmail.com>
2015-01-20 18:48:20 +0000

    custom bootstrap

--- code/config.py
+++ code/config.py
@@ -3,22 +3,20 @@ config file for grace app
 
 """
 
-#permits={} #basis of permit system
 
 sitename='change'
-guests=False #do guests have access by default?
-#guest_comments=False
-avatars=False
-attribution=True
-links=False
-
-#logo="deeplogo.png"
-
 #meta_description="" #site description text for search engines
 #meta_keywords="" #comma separated list of keywords for search engines
 
-#page_welcome=13 #welcome age for newly validated members to arrive at
+#permits={} #basis of permit system
+
+guests=False #do guests have access by default?
+attribution=True
+default_class="Page"
+registration_method="admin" # "admin" : admin has to register each user                                                                    
+                              # "approve" : online self registration with approval by admin 
 page_default=1 # 
+urlpath=""  
 
 #from base.data.schema import *  #for data definition
 #
@@ -30,7 +28,4 @@ page_default=1 #
 #class User(BaseUser):
 #  pass
 
-#pagetabs=True  
-urlpath='/versere'  
 
-registration_method='admin'
--- code/serve.py
+++ code/serve.py
@@ -1,5 +1,5 @@
 """
-deep app server script
+evoke app server script
 
 this is for serving a single app
 
@@ -7,6 +7,8 @@ this is for serving a single app
 
 #fix the path
 import os,sys
+#sys.path.append(os.path.abspath('.')) 
+#sys.path.append(os.path.abspath('../..'))
 sys.path.insert(0,os.path.abspath('.')) 
 sys.path.insert(1,os.path.abspath('../..'))
 
@@ -14,7 +16,7 @@ from twisted.application import service
 from base.serve import start
 
 ## Twisted requires the creation of the root-level application object to take place in this file
-application = service.Application("deepserver application")
+application = service.Application("evoke application")
 ## stitch it all together...
 start(application)
 
